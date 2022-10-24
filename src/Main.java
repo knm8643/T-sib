8b1a319432ff1a520e5f9cfbee35f03581360388
이충재 <knm8643@nate.com>
2022-10-24 20:42:16 +0900

    springBuild

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,5 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("what the fuck Test");
-        System.out.println("ha!ha!");
-        System.out.println("ssib T ssib");
     }
 }
